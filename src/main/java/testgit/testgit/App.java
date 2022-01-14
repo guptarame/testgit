088c03228ae4ff64142ea1b3a2e8bb68dc7aee12
guptarame <guptarame@gmail.com>
2022-01-15 00:54:05 +0530

    ok

--- src/main/java/testgit/testgit/App.java
+++ src/main/java/testgit/testgit/App.java
@@ -6,6 +6,6 @@ package testgit.testgit;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello Ramesh - World!");
+		System.out.println("Hello Ramesh QA - World!");
 	}
 }
